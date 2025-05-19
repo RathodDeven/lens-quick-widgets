2dc7baf47760f9d6a94ee35cd1c5f65d0ca730cb
Rathod Deven <devenrathodrd@gmail.com>
2025-05-20 01:26:01 +0530

    fix: remove unused IPFS and ImageKit endpoints, update NPM package link in config.ts

--- apps/web/src/utils/config.ts
+++ apps/web/src/utils/config.ts
@@ -1,8 +1,7 @@
 export const APP_NAME = 'Lens Quick Widgets'
-export const IPFS_ENDPOINT = 'https://4everland.io/ipfs/'
-export const IMAGE_KIT_ENDPOINT = 'https://ik.imagekit.io/kopveel8c'
 export const DEFAULT_THEME: 'light' | 'dark' = 'light'
 export const EXAMPLE_POST_ID = '121frwetbr02new3gvk'
 export const APP_LINK = 'https://your-domain.com'
 export const GITHUB_LINK = 'https://github.com/RathodDeven/lens-quick-widgets'
-export const NPM_PACKAGE_LINK = ''
+export const NPM_PACKAGE_LINK =
+  'https://www.npmjs.com/package/lens-quick-widgets'
