c751fe027b1efcee666d6430a642ecb2109082b8
Rathod Deven <devenrathodrd@gmail.com>
2025-05-20 01:43:30 +0530

    fix: restore unused IPFS and ImageKit endpoints in config.ts

--- apps/web/src/utils/config.ts
+++ apps/web/src/utils/config.ts
@@ -5,3 +5,5 @@ export const APP_LINK = 'https://your-domain.com'
 export const GITHUB_LINK = 'https://github.com/RathodDeven/lens-quick-widgets'
 export const NPM_PACKAGE_LINK =
   'https://www.npmjs.com/package/lens-quick-widgets'
+export const IPFS_ENDPOINT = 'https://4everland.io/ipfs/'
+export const IMAGE_KIT_ENDPOINT = 'https://ik.imagekit.io/kopveel8c'
